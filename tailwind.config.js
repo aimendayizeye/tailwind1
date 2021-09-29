ff806bbb50295a6518fc5b724b6131ca064b3430
Memento <salimalsalif@gmail.com>
2021-09-29 18:23:40 +0200

    Challenge 2

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {},
+    fontFamily:{
+     Poppins: ["Poppins,sans-serif"],
+    },  container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+       
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    
+  ],
+}
